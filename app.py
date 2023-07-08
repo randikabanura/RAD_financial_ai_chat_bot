63468453f230ed04f9e0fbd5800c871e3207c888
Banura Randika <randika.banura@gmail.com>
2023-07-08 13:25:03 +0530

    Chatbot API key update

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import openai
 import gradio as gr
-import random
 import time
 
 openai.api_key = ""
 
 messages = [
-    {"role": "system", "content": "You are an AI specialized in Financial Advice  in context of Sri Lankan investment and"
-                                  "wealth generation options. Do not answer anything other than food-related"
-                                  "queries."},
+    {"role": "system",
+     "content": "You are an AI specialized in Financial Advice  in context of Sri Lankan investment and"
+                "wealth generation options. Do not answer anything other than Financial Advice"
+                "queries."},
 ]
 
 questions = {
-    "What is langauge you prefer ?": "",
-    "Test": ""
+    "What is langauge you prefer ?": None,
+    "Are you comfortable with higher-risk investments for potentially higher returns, or do you prefer lower-risk "
+    "options?": None,
+    "Are you aiming for long-term wealth accumulation, retirement planning, or specific short-term objectives?": None
 }
+
+initial_message = "TEST TEST"
+
 
 def chatbot_response(input):
     if input:
@@ -26,21 +31,31 @@ def chatbot_response(input):
         messages.append({"role": "assistant", "content": reply})
         return reply
 
+
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
+    chatbot = gr.Chatbot(value=[[None, initial_message], [None, list(questions.keys())[0]]])
     msg = gr.Textbox()
     clear = gr.ClearButton([msg, chatbot])
 
     def respond(message, chat_history):
-        for item in questions.items():
-            chat_history.append((message, item.key))
-            
-        bot_message = chatbot_response(message)
-        chat_history.append((message, bot_message))
-        time.sleep(2)
-        return "", chat_history
+        for item in questions.keys():
+            if questions[item] is None:
+                questions[item] = message
+                break
 
-    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+        for item in questions.keys():
+            if questions[item] is None:
+                chat_history.append((message, item))
+                time.sleep(2)
+                return "", chat_history
+
+        # bot_message = chatbot_response(message)
+        # chat_history.append((message, bot_message))
+        # time.sleep(2)
+        # return "", chat_history
+
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot], scroll_to_output=True)
 
 if __name__ == "__main__":
     demo.launch()
